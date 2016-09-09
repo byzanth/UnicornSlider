0619a663c7a268ed3fc6615467a450dbcc67b350
nkr <nkr@netempire.de>
2016-09-09 11:58:38 +0200

    grunt automation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,61 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            compass: {
+                files: ['unicornslider.scss'],
+                tasks: ['compass:main','copy']
+            },
+            js: {
+                files: ['jquery.unicornslider.js'],
+                tasks: ['uglify','copy']
+            }
+        },
+        compass: {
+            main: {
+                options: {
+                    sourcemap: false,
+                    sassDir: ['.'],
+                    cssDir: ['.'],
+                    environment: 'production',
+                    outputStyle: "compressed",
+                    force: true
+                },
+                files: [
+                    {
+                        src: 'unicornslider.scss',
+                        dest: 'unicornslider.min.css'
+                    }
+                ]
+            }
+        },
+        uglify: {
+            main: {
+                files: {
+                    'jquery.unicornslider.min.js': ['jquery.unicornslider.js']
+                }
+            }
+        },
         copy: {
             main: {
                 files: [{
-                    expand: true,
-                    cwd: '',
-                    src: ['unicornslider.min.css'],
-                    dest: 'demo/css/'
-                },
-                {
-                    expand: true,
-                    cwd: '',
-                    src: ['jquery.unicornslider.min.js'],
-                    dest: 'demo/js/'
-                }]
+                        expand: true,
+                        cwd: '',
+                        src: ['unicornslider.min.css'],
+                        dest: 'demo/css/'
+                    },
+                    {
+                        expand: true,
+                        cwd: '',
+                        src: ['jquery.unicornslider.min.js'],
+                        dest: 'demo/js/'
+                    }]
             }
         }
     });
-
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.registerTask('default', ['copy']);
+    grunt.registerTask('default', ['compass:main', 'uglify', 'copy']);
 };
